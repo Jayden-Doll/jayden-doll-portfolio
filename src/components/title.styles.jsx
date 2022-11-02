035cf09a1ac63c497d67a6df785b3a7bf77cfbf7
Jayden Doll <jayjdoll002@gmail.com>
2022-11-02 09:43:46 -0700

    Update favicon

--- src/components/title.styles.jsx
+++ src/components/title.styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Heading = styled.h1`
   font-family: "JetBrains Mono";
-  background: -webkit-linear-gradient(-225deg, #22e1ff 0%, #4AE4A7 75%);
+  background: -webkit-linear-gradient(-225deg, #22e1ff 0%, #4ae4a7 75%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-weight: 400;
@@ -11,12 +11,12 @@ export const Heading = styled.h1`
 
 export const Subheading = styled.h2`
   font-family: "JetBrains Mono";
-  color: #C0EFFF;
+  color: #def7ff;
   font-weight: 300;
   margin: 0.5rem;
 `;
 
 // Colors:
-//C0EFFF Light blue
+//def7ff Light blue
 //4AE4A7 greenish blue
-//22e1ff cyan 
+//22e1ff cyan
